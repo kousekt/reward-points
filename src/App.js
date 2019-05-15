6d8ed6d689ab988e6bae5b9e2885ccbbca2f12e6
Theron Kousek <tkousek2002@yahoo.com>
2019-05-15 07:11:56 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import fetch from './api/dataService';
+import ReactTable from 'react-table';
+import "./App.css";
+
+function calculateResults(incomingData) {
+  // Calculate points per transaction
+
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const pointsPerTransaction = incomingData.map(transaction=> {
+    let points = 0;
+    let over100 = transaction.amt - 100;
+    
+    if (over100 > 0) {
+      // A customer receives 2 points for every dollar spent over $100 in each transaction      
+      points += (over100 * 2);
+    }    
+    if (transaction.amt > 50) {
+      // plus 1 point for every dollar spent over $50 in each transaction
+      points += 50;      
+    }
+    const month = new Date(transaction.transactionDt).getMonth();
+    return {...transaction, points, month};
+  });
+               
+  let byCustomer = {};
+  pointsPerTransaction.forEach(pointsPerTransaction => {
+    let {custid, name, month, points} = pointsPerTransaction;   
+    if (!byCustomer[custid]) {
+      byCustomer[custid] = [];      
+    }    
+    if (byCustomer[custid][month]) {
+      byCustomer[custid][month].points += points;
+    }
+    else {
+      byCustomer[custid][month] = {
+        custid,
+        name,
+        month: months[month],
+        points
+      }
+    }    
+  });
+  let tot = [];
+  for (var custKey in byCustomer) {    
+    byCustomer[custKey].forEach(cRow=> {
+      tot.push(cRow);
+    });    
+  }
+  console.log("byCustomer", byCustomer);
+  console.log("tot", tot);
+
+  return {
+    summaryByCustomer: tot
+  };
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [transactionData, setTransactionData] = useState(null);
+  
+  const columns = [
+    {
+      Header:'Customer',
+      accessor: 'name'
+    },    
+    {
+      Header:'Month',
+      accessor: 'month'
+    },
+    {
+      Header:'Reward Points',
+      accessor: 'points'
+    }
+  ]
+
+  useEffect(() => { 
+    fetch().then((data)=> {             
+      const results = calculateResults(data);
+      console.log("results", results);
+      setTransactionData(results);
+    });
+  },[]);
+
+  if (transactionData == null) {
+    return <div>Loading...</div>;   
+  }
+
+  return transactionData == null ?
+    <div>Loading...</div> 
+      :    
+    <div>
+      <h2>Points Rewards System</h2>
+
+      <ReactTable
+        data={transactionData.summaryByCustomer}
+        columns={columns}
+      />
     </div>
-  );
+  ;
 }
 
 export default App;
---
+++ src/api/dataService.js
@@ -0,0 +1,81 @@
+
+
+export default function() {
+  // simulates data coming from a database.
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "Acme",
+          amt: 120,
+          transactionDt: "05-01-2019"
+        },
+        {
+          custid: 1,
+          name: "Acme",
+          amt: 75,
+          transactionDt: "05-21-2019"
+        },
+        {
+          custid: 1,
+          name: "Acme",
+          amt: 10,
+          transactionDt: "06-01-2019"
+        },
+        {
+          custid: 1,
+          name: "Acme",
+          amt: 75,
+          transactionDt: "06-21-2019"
+        },
+        {
+          custid: 1,
+          name: "Acme",
+          amt: 200,
+          transactionDt: "07-01-2019"
+        },
+        {
+          custid: 1,
+          name: "Acme",
+          amt: 224,
+          transactionDt: "07-21-2019"
+        },
+        {
+          custid: 2,
+          name: "Century",
+          amt: 125,
+          transactionDt: "05-01-2019"
+        },
+        {
+          custid: 2,
+          name: "Century",
+          amt: 75,
+          transactionDt: "05-21-2019"
+        },
+        {
+          custid: 2,
+          name: "Century",
+          amt: 10,
+          transactionDt: "06-01-2019"
+        },
+        {
+          custid: 2,
+          name: "Century",
+          amt: 75,
+          transactionDt: "06-21-2019"
+        },
+        {
+          custid: 2,
+          name: "Century",
+          amt: 200,
+          transactionDt: "07-01-2019"
+        },
+        {
+          custid: 2,
+          name: "Century",
+          amt: 224,
+          transactionDt: "07-21-2019"
+        }
+    ]
+  );
+};
